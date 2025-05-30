8afc6104e5dd869364ed86040507023d8df11ec5
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-05-30 15:17:59 +0530

    ICEICO Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-// Components
 import Navbar from "./components/Navbar";
 import VideoHeader from "./components/VideoHeader";
 import AboutUs from "./components/AboutUs";
